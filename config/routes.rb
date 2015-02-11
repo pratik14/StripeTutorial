6be6b1b84aeb9e8a9b84c7d79f0679b9f9eab06b
Pratik Shah <pratik@joshsoftware.com>
2015-02-11 12:38:22 +0530

    Session controller added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
 
   get '/pricing', to: 'home#pricing'
   post '/subscribe', to: 'home#subscribe'
+
+  resource :sessions, only: [:new] do
+    member do
+      get :destroy, as: 'destroy'
+    end
+  end
   root :to => 'home#index'
 end
